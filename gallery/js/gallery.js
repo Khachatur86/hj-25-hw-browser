65bfa485b8823afde506afc36f2126eba4735a37
Хачатур <>
2018-09-02 14:51:23 +0300

    Third exercise solution with class

--- gallery/js/gallery.js
+++ gallery/js/gallery.js
@@ -38,3 +38,64 @@
     setCurrentPhoto();
   }
 })();
+
+// class Gallery {
+
+//     constructor(prevBtn, nextBtn, currentPhoto) {
+//         this.prevBtn = prevBtn;
+//         this.nextBtn = nextBtn;
+//         this.currentPhoto = currentPhoto;
+
+//         this.photoList = [];
+//         this.index = 0;
+
+//         this.prevBtn.onclick = () => {
+//             this.prevPhoto();
+//             this.setCurrentPhoto();
+//         };
+
+//         this.nextBtn.onclick = () => {
+//             this.nextPhoto();
+//             this.setCurrentPhoto();
+//         };
+//     }
+
+//     addPhotos(...photos) {
+//         this.photoList.push(...photos);
+//     }
+
+//     prevPhoto() {
+//         if (this.index <= 0) {
+//             this.index = this.photoList.length - 1;
+//         } else {
+//             this.index--;
+//         }
+//     }
+
+//     nextPhoto() {
+//         if (this.index >= this.photoList.length - 1) {
+//             this.index = 0;
+//         } else {
+//             this.index++;
+//         }
+//     }
+
+//     setCurrentPhoto() {
+//         this.currentPhoto.src = this.photoList[this.index];
+//     }
+
+// }
+
+// (function () {
+
+//     const gallery = new Gallery(
+//         document.getElementById('prevPhoto'),
+//         document.getElementById('nextPhoto'),
+//         document.getElementById('currentPhoto')
+//     );
+
+//     gallery.addPhotos(
+//         'i/breuer-building.jpg', 'i/guggenheim-museum.jpg', 'i/headquarters.jpg', 'i/IAC.jpg', 'i/new-museum.jpg'
+//     )
+
+// })();
